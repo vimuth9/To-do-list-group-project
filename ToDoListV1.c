94c44dd7c2ea8a93f4ff458855b737be43d6dce9
vimuth9 <vimuthwic3@gmail.com>
2024-08-14 18:42:18 +0530

    test

--- ToDoListV1.c
+++ ToDoListV1.c
@@ -229,7 +229,7 @@ void add_task()
 {
     printf("Functon to add new task to the list");
     printf("///////////guide 02///////////////");
-    printf("Press")
+    printf("Press");
     printf("1-/t List01 - ");
     printf("2-/t List02 - ");
     printf("3-/t List03 - ");
